6f7d5cc6f3a169376af46b634d171d7791f0b286
yang.sun <yang.sun@huadaocart.com>
2021-08-16 20:12:44 +0800

    1

--- src/element/index.js
+++ src/element/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 
 //导入弹窗提示组件
-import { Message, Form, FormItem, Input, Button } from 'element-ui'
+import { Message, Form, FormItem, Input, Button, Main, Aside, Header,Container } from 'element-ui'
 
+
+//全局组件注册
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Button)
+Vue.use(Main)
+Vue.use(Aside)
+Vue.use(Header)
+Vue.use(Container)
 
 
 Vue.prototype.$message = Message
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,58 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home_container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+<span>电商管理系统后台</span>
+      </div>
+      
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-  methods:{
-     logout(){
-          window.sessionStorage.clear()
-          this.$router.push('/login')
-     }
-  }
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+  },
 };
 </script>
 
 <style lang="less" scoped>
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex ;
+    align-items: center;
+    span{
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333747;
+}
+
+.el-main {
+  background-color: #fff;
+}
+
+.home_container {
+  height: 100%;
+}
 </style>
